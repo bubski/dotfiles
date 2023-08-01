39a0151183d4eb388479a8c1dc7301ce9505ba5f
Bartek Chlebek <bartek.chlebek@gmail.com>
2023-08-01 00:22:23 +0000

    Add l1 and ll1 aliases

--- stow/.config/bubski-dotfiles/aliases/aliases.zsh
+++ stow/.config/bubski-dotfiles/aliases/aliases.zsh
@@ -1,4 +1,7 @@
 alias cat='bat --style=plain --paging=never'
 alias glog="git log --graph --pretty=format:'%Cgreen%ad%Creset %C(auto)%h%d %s %C(bold black)<%aN>%Creset' --date=format-local:'%Y-%m-%d %H:%M (%a)'"
 
+alias l1='ls -1'
+alias ll1='ls -1a'
+
 source "${HOME}/.config/bubski-dotfiles/aliases/aliases-$(uname).zsh"
