2025f61948558857372c3bae0454c3db6588177b
Bartek Chlebek <bartek.chlebek@gmail.com>
2025-02-14 23:55:33 -0500

    Fix condition

--- base/aliases/aliases.zsh
+++ base/aliases/aliases.zsh
@@ -1,6 +1,6 @@
 alias glog="git log --graph --pretty=format:'%Cgreen%ad%Creset %C(auto)%h%d %s %C(bold black)<%aN>%Creset' --date=format-local:'%Y-%m-%d %H:%M (%a)'"
 
-if [[ command -v bat &>/dev/null ]]; then
+if [[ $(command -v bat &>/dev/null) ]]; then
     alias cat='bat --style=plain --paging=never'
 fi
 
